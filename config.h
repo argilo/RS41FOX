74fbba2ff3e34f076426dd3b706609185050e141
Clayton Smith <argilo@gmail.com>
2023-10-10 14:16:50 -0400

    Start configuring for CTF

--- config.h
+++ config.h
@@ -9,11 +9,11 @@
 
 
 //************GLOBAL Settings*****************
-#define TRANSMIT_FREQUENCY  434.650f // MHz. (Default is within the ISM band. ARDF Frequency in Australia is 439.4 MHz) 
-#define CALLSIGN "N0CALL" // Put your callsign here, max. 15 characters
+#define TRANSMIT_FREQUENCY  434.650f // MHz. (Default is within the ISM band. ARDF Frequency in Australia is 439.4 MHz)
+#define CALLSIGN "VE3IRR" // Put your callsign here, max. 15 characters
 
 // TX Power
-#define TX_POWER  5 // PWR 0...7 0- MIN ... 7 - MAX
+#define TX_POWER  0 // PWR 0...7 0- MIN ... 7 - MAX
 // Power Levels measured at 434.650 MHz, using a Rigol DSA815, and a 10 kHz RBW
 // Power measured by connecting a short (30cm) length of RG316 directly to the
 // antenna/ground pads at the bottom of the RS41 PCB.
@@ -29,20 +29,20 @@
 
 //*************BEACON SETTINGS****************
 // Morse Ident speed.
-#define MORSE_WPM	20
+#define MORSE_WPM	12
 
 // CW beacon on & off times.
-#define ON_TIME		10 // Seconds
-#define OFF_TIME	10 // Seconds
+#define ON_TIME		5 // Seconds
+#define OFF_TIME	20 // Seconds
 
 // Number of On/Off Repeats.
 #define ONOFF_REPEATS 1
 
-// Low-Voltage Beacon 
+// Low-Voltage Beacon
 // If enabled (uncommmented), beacon the lat/lon of the fox in the CW ident when the battery voltage gets low.
 // NOTE: If you are going to place the fox in a location with no GPS reception, disable this mode,
 // else it will draw lots of power trying to acquire GPS signal.
-#define LOW_VOLTAGE_BEACON 1
+// #define LOW_VOLTAGE_BEACON 1
 
 // Low-Voltage beacon threshold voltage. For ~4-5 hours of position beaconing before going flat, use:
 // Lithium AA: 2.7 volts  (Refer 100mA discharge curve here: http://data.energizer.com/pdfs/l91.pdf )
@@ -51,7 +51,7 @@
 
 // Low-Voltage Cutout - Kill power if the supply drops below this level. Useful to avoid destroying cells.
 // Set this to < 1 Volt for (essentially) no low-voltage cutout.
-#define LOW_VOLTAGE_CUTOUT 0.5 // Volts
+#define LOW_VOLTAGE_CUTOUT 0.0 // Volts
 
 
 #endif
